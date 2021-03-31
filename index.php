a44637b9c2d3450fd7e07f1192d9dd48817a3ace
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-03-31 10:29:39 +0100

    login page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include_once('connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="proflogin.php" method="POST">
+        <label for="email">Email</label><br>
+        <input type="mail" name="email"><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password"><br><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+</html>
