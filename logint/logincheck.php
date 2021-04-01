cfd179878100e82d7f99955a77df5c1fdd6b566a
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-04-01 10:21:41 +0100

    create login

---
+++ logint/logincheck.php
@@ -0,0 +1,19 @@
+<?php
+include_once('connection.php');
+
+if (isset($_POST["submit"])) {
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $query = "SELECT * FROM `users` WHERE email='$email' && password='$password'";
+    $login = mysqli_query("$conn", "$query");
+
+    $count = mysqli_num_rows($login);
+
+    if ($count > 0) {
+        header('Location:index.php');
+    } else {
+        echo "Your password incorect";
+    }
+}
