a7adaedc21a0a59c8dbae9bcd7e0d7fecb0aad57
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-04-01 23:19:31 +0100

    change finale

--- dashboard-prof/cma/core/database.php
+++ dashboard-prof/cma/core/database.php
@@ -1,12 +1,12 @@
 <?php
-define("BASE_URL","http://localhost");
-define('HOST',"localhost");
-define('USER',"root");
-define('PASSWORD',"");
-define('DATABASE',"cma");
+define("BASE_URL", "http://localhost");
+define('HOST', "localhost");
+define('USER', "root");
+define('PASSWORD', "");
+define('DATABASE', "cma");
 
-$con = mysqli_connect(HOST,USER,PASSWORD,DATABASE);
-if(mysqli_errno($con)){
+$con = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+if (mysqli_errno($con)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
 }
