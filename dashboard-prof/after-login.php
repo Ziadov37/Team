a3d1e76473785db5fb520008c5b13f04cbf489b6
Akram <akram.iguernait@gmail.com>
2021-04-01 16:34:46 +0100

    added code for after login choice

---
+++ dashboard-prof/after-login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="afterLogin.css">
+    <title>choose</title>
+</head>
+<body>
+    <div class="container">
+        <p>Create Student Accounts :</p>
+      
+        <button class="button" type="button"><a href="index.php">Click Here</a></button>
+      
+      </div>
+      
+      <div class="container container--inverted">
+        <p>Create Brief :</p>
+        
+        <button class="button" type="button"><a href="create_brief.php">Click Here</a></button>
+        
+      </div>
+</body>
+</html>
