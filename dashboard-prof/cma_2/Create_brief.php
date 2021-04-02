cdc5e7405a54e397715069b8919ae923f3d766b6
yahya <yahyaelhabchi476@gmail.com>
2021-04-02 15:11:31 +0100

    add design simple

--- dashboard-prof/cma_2/Create_brief.php
+++ dashboard-prof/cma_2/Create_brief.php
@@ -14,7 +14,7 @@ if (isset($_POST['Send'])) {
         $proofId = 1; //$_SESSION['proof-id'] = ?;
         $query = "INSERT INTO `brief` ( title, description,createdat,deadline,idproof) VALUES ('$title', '$url',CURDATE(),'$deadline','$proofId')";
         $result = mysqli_query($con, $query) or die(mysqli_error($con));
-        header('Location:index.php');
+        header('Location:Create_brief.php');
     } else {
         echo 'Error';
         var_dump($_POST);
@@ -38,20 +38,35 @@ if (isset($_POST['Send'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Create Brief </h1>
+    <h1 class="text-center p-3 mb-4 ">Create Brief </h1>
 
-    <form action="" method="POST">
+    <form action="" method="POST" class="text-center p-3 mb-4 ">
         <label for="title">Add your Title:</label>
-        <input type="text" id="title" name="title" placeholder="Enter Title"><br><br>
+        <input class="form-control m-3" type="text" id="title" name="title" placeholder="Enter Title"><br><br>
         <label for="url">Add your Url:</label>
-        <input type="text" id="url" name="url" placeholder="Enter Url"><br><br>
+        <input class="form-control m-3" type="text" id="url" name="url" placeholder="Enter Url"><br><br>
         <label for=""> deadline </label>
-        <input type="date" name="deadline">
-        <input type="submit" name="Send" value="Send">
+        <input class="form-control m-3" type="date" name="deadline">
+        <input class="btn btn-dark m-5 " type="submit" name="Send" value="Send">
     </form>
+
+
+
+
+
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- dashboard-prof/cma_2/show.php
+++ dashboard-prof/cma_2/show.php
@@ -11,18 +11,34 @@ $results = mysqli_query($con, $query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 
 <body>
-    <ul>
+    <ul class="d-flex justify-content-between m-5">
         <?php foreach ($results as $row) : ?>
-        <li class="brief"><a href="<?php echo $row["description"];  ?>"> <?php echo $row["title"];  ?> </a></li>
+        <li class="list-group-item"><a href=" <?php echo $row["description"];  ?>" target="_blank" class="btn btn-dark">
+                <?php echo $row["title"];  ?> </a>
+        </li>
+
         <?php endforeach ?>
     </ul>
+
+
+
+
+
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
 </body>
-
-
-
 
 </html>
