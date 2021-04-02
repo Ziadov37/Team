bddd2e278b4247f31789edb871a66939421bee4d
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-04-02 10:07:09 +0100

    Modification on student dashboard

---
+++ logint/logincheck.php
@@ -0,0 +1,47 @@
+<?php
+include_once('connection.php');
+
+if (isset($_POST["submit"])) {
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $query = "SELECT * FROM proof WHERE email='" . $email . "' ";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if ($row["password"] == $password) {
+            header('Location:../dashboard-prof/after-login.php');
+        } else {
+            header('Location:index.php?login=ko');
+        }
+    } else {
+        $query = "SELECT * FROM students WHERE email='" . $email . "' ";
+        $result = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row["password"] == $password) {
+                header('Location:../dashboard-prof/cma_2/show.php');
+            } else {
+                header('Location:index.php?login=ko');
+            }
+        } else {
+            header('Location:index.php?login=ko');
+        }
+    }
+}
+
+
+
+// if (isset($_POST["submit"])) {
+//     $email =  $_POST['email'];
+//     $password =  $_POST['password'];
+//     $query =  "SELECT * FROM `users` WHERE email='$email' && password='$password'";
+//     $login = mysqli_query($conn, $query);
+//     if (mysqli_num_rows($login) > 0) {
+//         header('Location:dashboardt/index.php');
+//     } else {
+//         echo "Your password incorect";
+//     }
+// }
